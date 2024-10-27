88bedabf60b05cf07e8da2ca185f53a98b0ddab1
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-10-27 09:38:29 +0300

    Adding Letter Placeholders For The Word & Letter Style

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const letters = document.querySelector('.letter-placeholders')
+
 fetch("./words.json")
   .then((response) => response.json())
   .then((data) => {
     const randomWord = data.words[Math.floor(Math.random() * data.words.length)];
     console.log(randomWord); // Might output "blue", "green", etc.
+
+    for (let i=0 ; i<randomWord.length ; i++) {
+        const span = document.createElement('span')
+        span.className = 'letter'
+        span.dataset.letter = randomWord[i]
+
+        letters.append(span)
+    }
   })
