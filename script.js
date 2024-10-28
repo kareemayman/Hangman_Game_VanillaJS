80ace5fa267be57f34f61cd208251df1dff3ac8b
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-10-28 08:30:12 +0300

    Hid The Letters, Added Animation, Added Event on Key Pressdown, & Finshed Repeated Letter Msg.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const letters = document.querySelector('.letter-placeholders')
+const pressedBeforeMsg = document.querySelector('.pressed-before')
 
 fetch("./words.json")
   .then((response) => response.json())
@@ -14,3 +15,50 @@ fetch("./words.json")
         letters.append(span)
     }
   })
+
+let lettersTried = []
+let wrongAnswers = []
+
+// Event For Pressing a Key
+document.addEventListener("keyup", (e) => {
+
+  // Check If User Pressed a Letter
+  let regExp = /^[a-z]$/img
+  if (regExp.test(e.key)) {
+
+    // Check If Letter Was Pressed Before To Print Message
+    if (lettersTried.includes(e.key)) {      
+      pressedBefore()
+    } else {
+
+      // Get All Letter Placeholder Spans
+      const letterSpans = Array.from(document.querySelectorAll(".letter-placeholders span"))
+
+      let found = false
+
+      // Check If The Letter Is Found In The Word & Make It Visible
+      letterSpans.forEach((span) => {
+        if (span.dataset.letter === e.key) {
+          span.classList.add("letter-visible")
+          found = true
+        }
+      })
+
+      // Check If The Letter Was Wrong
+      if (found === false) {
+        wrongAnswers.push(e.key)
+      }
+
+      // Add Letter To Tried Letters List
+      lettersTried.push(e.key)
+    }
+  }
+})
+
+// Function For Message When User Tries Same Letter Again
+function pressedBefore() {
+  pressedBeforeMsg.classList.add('pressed-before-visible')
+  setTimeout(() => {
+    pressedBeforeMsg.classList.remove('pressed-before-visible')
+  }, 2000);
+}
